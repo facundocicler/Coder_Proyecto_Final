1118ab5d2e3d072fd0461cd7de9e6c5325bc64f6
Facundo Cicler <119620428+facundocicler@users.noreply.github.com>
2024-05-14 12:03:40 -0300

    Actualizar Alert_Msg.py

--- dags/Alert_Msg.py
+++ dags/Alert_Msg.py
@@ -49,3 +49,37 @@ def check_and_send_alert(**context):
 
     else:
         print("No se requiere alerta de temperatura.")
+
+def send_alert(**context):
+
+csv_filename = context['ti'].xcom_pull(task_ids='extract_data')
+    df = pd.read_csv(csv_filename)
+    if alert_messages:
+        alert_body = "\n".join(alert_messages)
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587
+        sender_email = Variable.get("EMAIL")
+        password = Variable.get("EMAIL_PASSWORD")
+
+        subject = f'Alerta de Temperatura de Ciudades Fuera del Rango de {min_temp} °C - {max_temp} °C'
+        message = alert_body
+
+        try:
+            body_text = f"\n{message}"
+            msg = MIMEMultipart()
+            msg['To'] = sender_email
+            msg['From'] = sender_email
+            msg['Subject'] = subject
+            msg.attach(MIMEText(body_text, 'plain'))
+
+            with smtplib.SMTP(smtp_server, smtp_port) as server:
+                server.starttls()
+                server.login(sender_email, password)
+                server.send_message(msg)
+            print('El email fue enviado con éxito')
+
+        except Exception as e:
+            print(f'Error al enviar el correo electrónico: {str(e)}')
+
+    else:
+        print("No se requiere alerta de temperatura.")
