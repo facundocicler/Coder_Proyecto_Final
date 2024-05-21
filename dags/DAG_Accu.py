de80b98ffc62bb54de2d03c12413f1a0072a0193
Facundo Cicler <119620428+facundocicler@users.noreply.github.com>
2024-05-21 19:47:29 -0300

    Update DAG_Accu.py

--- dags/DAG_Accu.py
+++ dags/DAG_Accu.py
@@ -4,7 +4,7 @@ from airflow.operators.python_operator import PythonOperator
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from airflow.models import Variable
-from AccuWeather_ETL import get_data, tables_redshift, insert_data
+from AccuWeather_ETL import extract_data, tables_redshift, insert_data
 from Alert_Msg import check_weather, send_alert
 import os
 import pandas as pd
@@ -27,58 +27,6 @@ weather_dag = DAG(
     schedule_interval=timedelta(days=1),
     catchup=False
 )
-
-def check_and_send_alert(**context):
-    csv_filename = context['ti'].xcom_pull(task_ids='extract_data')
-    df = pd.read_csv(csv_filename)
-
-    with open('dags/config.json', 'r') as json_config:
-        try:
-            config_data = json.load(json_config)
-            min_temp = config_data['celsius_temperature_range']['min_temp']
-            max_temp = config_data['celsius_temperature_range']['max_temp']
-        except Exception as e:
-            print(f'Error al cargar el archivo JSON: {str(e)}')
-
-    city = df['city']
-    country = df['country']
-    temperature = df['temperature_metric_value']
-    alert_messages = []
-
-    for city_name, country_name, temp in zip(city, country, temperature):
-        if temp < min_temp or temp > max_temp:
-            alert_message = f"{city_name}, {country_name} - {temp} °C"
-            alert_messages.append(alert_message)
-
-    if alert_messages:
-        alert_body = "\n".join(alert_messages)
-        smtp_server = 'smtp.gmail.com'
-        smtp_port = 587
-        sender_email = Variable.get("EMAIL")
-        password = Variable.get("EMAIL_PASSWORD")
-
-        subject = f'Alerta de Temperatura de Ciudades Fuera del Rango de {min_temp} °C - {max_temp} °C'
-        message = alert_body
-
-        try:
-            body_text = f"\n{message}"
-            msg = MIMEMultipart()
-            msg['To'] = sender_email
-            msg['From'] = sender_email
-            msg['Subject'] = subject
-            msg.attach(MIMEText(body_text, 'plain'))
-
-            with smtplib.SMTP(smtp_server, smtp_port) as server:
-                server.starttls()
-                server.login(sender_email, password)
-                server.send_message(msg)
-            print('El email fue enviado con éxito')
-
-        except Exception as e:
-            print(f'Error al enviar el correo electrónico: {str(e)}')
-
-    else:
-        print("No se requiere alerta de temperatura.")
 
 check_weather_task = PythonOperator(
     task_id='check_weather',
